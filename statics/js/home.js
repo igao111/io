7a13261c6046ce97ee892f7e47824e82732037c2
igao111 <70744089+igao111@users.noreply.github.com>
2021-03-27 21:55:51 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('http://igao1.com', "_blank");
+            window.open('https://igao1.com', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('http://igao2.com', "_blank");
+            window.open('https://igao2.com', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://igao3.com', "_blank");
+            window.open('https://igao3.com', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('http://igao5.com', "_blank");
+            window.open('https://igao111.com', "_blank");
         }
         document.getElementById("btnEnter6").onclick = function() {
-            window.open('http://www.google.com', "_blank");
+            window.open('https://www.google.com', "_blank");
         }
     }
     function init() {
