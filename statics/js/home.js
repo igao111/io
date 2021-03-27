57f9082bc590c926bb23f94cce0292f88e8221e0
igao111 <70744089+igao111@users.noreply.github.com>
2021-03-27 16:11:36 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('http://www.igao1.com', "_blank");
+            window.open('http://igao1.com', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('http://www.igao2.com', "_blank");
+            window.open('http://igao2.com', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://www.igao3.com', "_blank");
+            window.open('http://igao3.com', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('http://www.igao111.com', "_blank");
+            window.open('http://igao111.com', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('http://www.igao555.com', "_blank");
+            window.open('http://igao555.com', "_blank");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.google.com', "_blank");
