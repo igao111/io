0c8bc1e193a39ea9eb5b845c5ce7dce7d6167adc
igao111 <70744089+igao111@users.noreply.github.com>
2021-03-27 15:53:42 +0800

    Add files via upload

---
+++ statics/js/home.js
@@ -0,0 +1,26 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('http://www.igao1.com', "_blank");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('http://www.igao2.com', "_blank");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('http://www.igao3.com', "_blank");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://www.igao111.com', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
+            window.open('http://www.igao555.com', "_blank");
+        }
+        document.getElementById("btnEnter6").onclick = function() {
+            window.open('http://www.google.com', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
