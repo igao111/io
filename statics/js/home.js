490bdad4f8af8eaefc485744293347bd3d2286b1
igao111 <70744089+igao111@users.noreply.github.com>
2021-06-06 17:11:33 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://igao999.com', "_self");
+            window.open('https://igao12.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://igao2.com', "_self");
+            window.open('https://igao13.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://igao3.com', "_self");
+            window.open('https://igao14.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://igao111.com', "_self");
+            window.open('https://igao15.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.meizuhui.com/', "_blank");
