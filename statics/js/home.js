e94604912de12f97ba569180f25d1876da807863
igao111 <70744089+igao111@users.noreply.github.com>
2022-05-16 22:54:34 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://61.igao57.com/', "_self");
+            window.open('https://65.igao57.com/', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://62.igao57.com/', "_self");
+            window.open('https://66.igao57.com/', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://63.igao57.com/', "_self");
+            window.open('https://67.igao57.com/', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://64.igao57.com/', "_self");
+            window.open('https://68.igao57.com/', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('https://www.baidu.com', "_blank");
